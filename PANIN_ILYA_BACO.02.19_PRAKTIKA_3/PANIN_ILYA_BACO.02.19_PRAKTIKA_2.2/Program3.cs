ba1fd79a8a74a6346cb6d3034620ff5dac14fb66
PaNuSHa <70700507+panusha-official@users.noreply.github.com>
2020-09-03 17:38:53 +0300

    Add files via upload

---
+++ PANIN_ILYA_BACO.02.19_PRAKTIKA_3/PANIN_ILYA_BACO.02.19_PRAKTIKA_2.2/Program3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PANIN_ILYA_BACO._02._19_PRAKTIKA_2._2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string s = "Microsoft .NET Framework 2 Application Development Foundation";
+      string[] sa = s.Split(" ");
+
+      Array.Sort(sa);
+
+      s = string.Join(" ", sa); 
+      Console.WriteLine(s);
+    }
+  }
+}
